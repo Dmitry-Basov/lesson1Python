0340893627ab7092493774680c4bf4756991bc51
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-30 01:23:59 +0300

    lesson1 + sem 1

---
+++ main.py
@@ -0,0 +1,60 @@
+# print('Введите первую число: ')
+# a = input()
+
+# b = input('введите второе число: ')
+
+# print(a, ' + ', b, ' = ', a + b)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c)
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# print('Введите первую число: ')
+# a = int (input())
+
+# b = int (input('введите второе число: '))
+
+# print(a, ' + ', b, ' = ', a + b)
+
+# f = 5.6888
+# b = 6.4646
+# print(round(f*b, 3))
+
+# iter = 2
+# iter += 3 # iter =iter + 3
+# iter -= 4 # iter = iter -4
+# iter *= 5 # iter = iter * 5
+# iter /= 5 # iter = iter / 5
+# iter //= 5 # iter = iter // 5
+# iter %= 5 # iter = iter % 5
+# iter **= 5 # iter = iter ** 5 - возведение в степень
+
+# i = int(input())
+# j = int(input())
+
+# if i -j == 0:
+#     Print(-1)
+# else:
+#     print(i + j - 1)
+
+# Дано натуральное число. Требуется определить, является ли год високосным.
+# Если год является високосным, то выведите YES, иначе выведите NO. Напомним,
+# что в соответствии с грирорианским календарем, год является високосным, если
+# его номер кратен 4, но не кратен 100, а также если он кратен 400.
+
+# n = int(input())
+
+# if n % 4 == 0 and n % 100 != 0 or n % 400 == 0:
+#     print("Yes")
+# else:
+#     print("No")
